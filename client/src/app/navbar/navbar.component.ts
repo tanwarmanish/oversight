54bc0906b0a3b619125ea90a5997e26308a62593
Manish Tanwar <mkumar@cyzerg.com>
2024-06-26 23:36:50 +0530

    navbar updated

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,26 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent {
   title = 'client';
+
+  links = [
+    {
+      icon: 'fa-wallet',
+      route: '',
+      title: 'Wallet'
+    },
+    {
+      icon: 'fa-money-bill-1',
+      route: '',
+      title: 'Budget'
+    }, {
+      icon: 'fa-piggy-bank',
+      route: '',
+      title: 'Goals'
+    }
+  ]
 }
