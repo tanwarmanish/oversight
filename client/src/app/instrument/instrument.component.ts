16281f616c7f62525ff6aa67f188dd381f30facb
Manish Tanwar <mkumar@cyzerg.com>
2024-06-26 23:20:58 +0530

    Tailwind added

---
+++ client/src/app/instrument/instrument.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'instrument',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './instrument.component.html',
+  styleUrl: './instrument.component.scss'
+})
+export class InstrumentComponent {
+  title = 'client';
+}
